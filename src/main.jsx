1b938ec2f3b40ae52e5ba20b4791bfe185e727c1
Amit Padhihari <amitpadhihari2004@gmail.com>
2026-06-21 21:55:08 +0530

    Problem solved

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import UserProvider from "./context/UserContext";
+import UserProvider from "./context/userContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
